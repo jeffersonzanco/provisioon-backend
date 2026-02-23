16b7d21514782f82760b20dd6d2b628823718cf4
jeffersonzanco <jeffersonzancousa@gmail.com>
2026-02-22 19:56:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -166,6 +166,6 @@ app.post('/api/send-key', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('Server is running on port ' + PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
